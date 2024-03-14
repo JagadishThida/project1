5c84b071edcfb7d48a761e808b6b6f138fa27cab
JagadishThida <163244243+JagadishThida@users.noreply.github.com>
2024-03-14 22:43:39 +0530

    Update stopcontainer.sh

--- scripts/stopcontainer.sh
+++ scripts/stopcontainer.sh
@@ -5,6 +5,7 @@ set -e
 #docker ps -q --filter "publish=<5000>" | xargs -r docker rm -f
 port=<5000>
 container_id=$(docker ps -q --filter "publish=${port}")
+- echo container_id
 if [ -n "$container_id" ]; then
   docker stop "$container_id"
   docker rm "$container_id"
